38bb4470634dbf8124acd7cea45611bb4aa51bb3
unknown <shoaibsarkarakash019@gmail.com>
2023-09-25 00:30:39 +0600

    Create banner

---
+++ src/Component/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+
+
+
+
+const Banner = () => {
+    return (
+      
+    <div>
+       
+            <div className="hero min-h-screen" style={{backgroundImage: 'url(https://i.ibb.co/8YcSngx/markus-winkler-w-LBVAF-k-MR0-unsplash.jpg)'}}>
+  <div className="hero-overlay bg-opacity-80"></div>
+  <div className="hero-content text-center text-neutral-content">
+   
+    <div className="">
+    <p className="text-5xl text-black font-bold mb-5">I Grow By Helping People In Need</p>
+    <div className="flex">
+    <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+    <button className="btn btn-primary">Search</button>
+    </div>
+    </div>
+  </div>
+</div>
+    </div>
+    );
+};
+
+export default Banner;
